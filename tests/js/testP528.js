393f62d988594f0a1943a14885ae22a1e160a7bb
Michael Rohner <mrohner@lstelcom.com>
2022-10-01 19:39:18 +0200

    add logging to unit test

--- tests/js/testP528.js
+++ tests/js/testP528.js
@@ -45,7 +45,11 @@ describe("Testing the model P.528", () => {
 
         const requestStr = JSON.stringify(request);
 
+        console.log("request: " + requestStr + "\n");
+
         const outputJson = Module.ccall('calcP528', 'string', ['string'], [requestStr]);
+
+        console.log("output: " + outputJson + "\n");
 
         const expectedJson = "{\"A__db\":[126.46085394936013,163.0815203486053,236.28197179677775,268.19292392138044,295.0633534171327,323.82517691745284],\"A_a__db\":0,\"A_fs__db\":[126.425056055803,157.56187253649026,163.55576213633907,167.04435177821983,169.5660332409185,171.5603915860845],\"ModeOfPropagation\":[1,1,3,3,3,3],\"d__km\":[0.0,360.0008519649057,720.0,1080.0,1440.0,1800.0],\"theta_h1__rad\":0,\"warnings\":0}";
 
